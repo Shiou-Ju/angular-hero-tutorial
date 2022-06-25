2b3b79fea4fde29224b56462bba4d008b54cb0a3
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-06-25 11:55:07 +0800

    feat: init mission detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MissionsComponent } from './missions/missions.component';
+import { MissionDetailComponent } from './mission-detail/mission-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, MissionsComponent],
+  declarations: [AppComponent, MissionsComponent, MissionDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/mission-detail/mission-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-detail',
+  templateUrl: './mission-detail.component.html',
+  styleUrls: ['./mission-detail.component.css']
+})
+export class MissionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
