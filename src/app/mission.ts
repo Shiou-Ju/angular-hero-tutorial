94b466e8ade6156338818046ebfc2623e68f1c7a
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-06-26 18:03:39 +0800

    feat: adjust mission interface & delete moc data

--- src/app/mission.ts
+++ src/app/mission.ts
@@ -1,6 +1,19 @@
 export interface Mission {
+  /** 任務序號 */
   id: number;
+  /** 例如：伏地挺身、練習英文 */
   name: string;
+  /** 單位，例如「組」 */
+  unit: string;
   /** 是否為每日定量 */
   isFixed: boolean;
+  /** 是否為定期增量 */
+  increment?: number;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  // TODO: encrypt password if possible
+  password: string;
 }
