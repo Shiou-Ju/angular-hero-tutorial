d43b4105ae3cdd1a6dcc609345ab3efbcfe99964
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-07-24 14:20:47 +0800

    feat: change titles of index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '三十七倍：原則';
+  title = 'Principle: 1.01 Daily Missions';
 }
